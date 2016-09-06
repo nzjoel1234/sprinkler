e8a4f46f3c7e743f9a5337ca331c61d53effc731
Joel <joel.mcbreen@gmail.com>
2016-09-06 21:33:31 +1200

    changes after tour of heroes tutorial and adding ng2-bootstrap

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
+    <alert type="info">ng2-bootstrap hello world!</alert>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+
+  title = 'Tour of Heroes';
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,33 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
+
+import { AppComponent }        from './app.component';
+import { routing }             from './app.routing';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [
+    BrowserModule,
+    Ng2BootstrapModule,
+    FormsModule,
+    routing
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
